dbb9eb3d2b9f6bb67e8ee12bfccd075728875d3a
BPplays <andymalbp@gmail.com>
2024-04-30 03:44:15 -0700

    changed controls and fit

--- mokuro/script.js
+++ mokuro/script.js
@@ -318,7 +318,7 @@ function zoomFitToWidth() {
 
 function zoomFitToScreen() {
   let scale_x = getScreenWidth() / pc.offsetWidth;
-  let scale_y = getScreenHeight2() / pc.offsetHeight;
+  let scale_y = (getScreenHeight2()+getOffsetTop2()) / pc.offsetHeight;
   let scale = (1 / pz.getTransform().scale) * Math.min(scale_x, scale_y);
   pz.moveTo(0, 0);
   pz.zoomTo(0, 0, scale);
