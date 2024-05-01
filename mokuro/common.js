32ecc7b4d6e24eafbd95fa0d8bc3eee85657444c
BPplays <andymalbp@gmail.com>
2024-04-30 20:46:32 -0700

    changed controls and fit

--- mokuro/common.js
+++ mokuro/common.js
@@ -315,6 +315,33 @@ function handleResize() {
 // Add event listener for the resize event
 window.addEventListener('resize', handleResize);
 
+function toggleFullScreen() {
+  var doc = window.document;
+  var docEl = doc.documentElement;
+
+  var requestFullScreen =
+    docEl.requestFullscreen ||
+    docEl.mozRequestFullScreen ||
+    docEl.webkitRequestFullScreen ||
+    docEl.msRequestFullscreen;
+  var cancelFullScreen =
+    doc.exitFullscreen ||
+    doc.mozCancelFullScreen ||
+    doc.webkitExitFullscreen ||
+    doc.msExitFullscreen;
+
+  if (
+    !doc.fullscreenElement &&
+    !doc.mozFullScreenElement &&
+    !doc.webkitFullscreenElement &&
+    !doc.msFullscreenElement
+  ) {
+    requestFullScreen.call(docEl);
+  } else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 document.addEventListener('keydown', function onEvent(e) {
   switch (e.key) {
     case "ArrowRight":
