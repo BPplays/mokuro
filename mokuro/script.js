c2301c277e0903cec7d1f320f7bdaaf00da73c1c
BPplays <andymalbp@gmail.com>
2024-04-30 20:45:27 -0700

    changed controls and fit

--- mokuro/script.js
+++ mokuro/script.js
@@ -44,15 +44,7 @@ function updateUI() {
   document.getElementById('menuPreloadAmount').value = state.preloadAmount;
 }
 
-function toggleFullscreen() {
-  if (!document.fullscreenElement) {
-      fullscreenDiv.requestFullscreen().catch(err => {
-          alert(`Error attempting to enable fullscreen mode: ${err.message} (${err.name})`);
-      });
-  } else {
-      document.exitFullscreen();
-  }
-}
+
 
 document.addEventListener(
   'DOMContentLoaded',
